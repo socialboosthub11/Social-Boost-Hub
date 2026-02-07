4c04324ab0f7dd6cdbb4d6cd757cd75c4164769b
Social Boost Hub <hafsatrusted@gmail.com>
2026-02-08 00:25:36 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '☣︎𝐒𝐎𝐂𝐈𝐀𝐋 𝐁𝐎𝐎𝐒𝐓 𝐇𝐔𝐁☣︎',
+  author: '‎',
+  botName: "☣︎𝐒𝐎𝐂𝐈𝐀𝐋 𝐁𝐎𝐎𝐒𝐓 𝐇𝐔𝐁☣︎",
+  botOwner: '☣︎𝘾𝙃𝙐𝙋𝙄𝙄 𝙍𝙐𝙎𝙏𝘼𝙈☣︎', 
+  ownerNumber: '923018968880', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/socialboosthub11/Social-Boost-Hub",
+};
+
+module.exports = settings;
